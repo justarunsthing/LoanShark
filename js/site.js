9034cb71d84e28bb70415c312ddc16659a341e34
justarunsthing <justarunsthing@outlook.com>
2025-05-21 20:07:03 +0100

    Added script for loan calculation

--- js/site.js
+++ js/site.js
@@ -1,3 +1,18 @@
-function helloWorld() {
-    alert("Hello World!");
+function getValues() {
+    let loanAmount = document.getElementById("loan-amount").value;
+    let term = document.getElementById("term").value;
+    let interestRate = document.getElementById("interest-rate").value;
+
+    loanAmount = parseInt(loanAmount);
+    term = parseInt(term);
+    interestRate = parseInt(interestRate);
+
+    // Calculate interest & cost
+    let totalInterest = loanAmount * (interestRate / 100) * (term / 12);
+    let totalCost = loanAmount + totalInterest;
+
+    document.getElementById("total-principal").innerHTML = `Total loan amount: ${loanAmount}`;
+    document.getElementById("total-interest").innerHTML = `Total interest: ${totalInterest}`;
+    document.getElementById("total-cost").innerHTML = `Total cost: ${totalCost}`;
+
 }
